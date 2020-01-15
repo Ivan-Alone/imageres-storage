0f666c2b2e300def4cae2afbc92a981f0d682a8a
Ivan <ivan-my-mail@yandex.com>
2020-01-15 23:52:13 +0300

    Update install.1.sh

--- install.1.sh
+++ install.1.sh
@@ -39,40 +39,10 @@ apt install -y xvfb
 apt install -y x11-xkb-utils
 apt install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
 apt install -y xserver-xorg-core
-if [[ "$(ls -l  /var/www/ | grep phpMyAdmin)" == '' ]]
-then
-	wget https://files.phpmyadmin.net/phpMyAdmin/5.0.0/phpMyAdmin-5.0.0-all-languages.zip && unzip phpMyAdmin-5.0.0-all-languages.zip && rm -rf phpMyAdmin-5.0.0-all-languages.zip && mv phpMyAdmin-5.0.0-all-languages /var/www/phpMyAdmin && mkdir /var/www/phpMyAdmin/tmp/ && chmod 777 /var/www/phpMyAdmin/tmp/ && cp /var/www/phpMyAdmin/config.sample.inc.php /var/www/phpMyAdmin/config.inc.php && echo "\$cfg['blowfish_secret'] = 'skdpofkmadsfg80456asp[osdfghsyrjfdaeSDffdgasnm0-9wh4n9';" >> /var/www/phpMyAdmin/config.inc.php
-	echo "Listen 1500
-
-<VirtualHost *:1500>
-    ServerName phpMyAdminPanel
-    ServerAdmin admin@phpMyAdminPanel
-    AddDefaultCharset UTF-8
-    DocumentRoot /var/www/phpMyAdmin
-    CustomLog /var/www/phpMyAdmin/access.log combined
-    ErrorLog /var/www/phpMyAdmin/error.log
-    DirectoryIndex index.php
-    AddDefaultCharset UTF-8
-    RewriteEngine On
-    RewriteCond %{REQUEST_URI} /var/www/phpMyAdmin/(.*)
-    RewriteRule ^(.*)\$ %1 [R]
-</VirtualHost>
-
-<Directory /var/www/phpMyAdmin>
-    Order allow,deny
-    Allow From All
-    Options +Includes +ExecCGI
-    RewriteEngine on
-    AllowOverride All
-    Require all granted
-</Directory>" > /etc/apache2/sites-available/phpmyadmin.conf
-	ln -s /etc/apache2/sites-available/phpmyadmin.conf /etc/apache2/sites-enabled/phpmyadmin.conf
-	ln -s /etc/apache2 /etc/httpd
-	ln -s /etc/apache2/apache2.conf /etc/apache2/httpd.conf
-	mysql -uroot -e "CREATE USER 'username'@'localhost' IDENTIFIED BY 'username_password_first_setup'; GRANT ALL PRIVILEGES ON * . * TO 'username'@'localhost'; FLUSH PRIVILEGES;"
-	a2enmod proxy rewrite proxy_http ssl
-	apachectl restart
-fi
+ln -s /etc/apache2 /etc/httpd
+ln -s /etc/apache2/apache2.conf /etc/apache2/httpd.conf
+a2enmod proxy rewrite proxy_http ssl
+apachectl restart
 
 
 # Mouse in terminal
@@ -95,7 +65,7 @@ apt update && apt install -y adoptopenjdk-8-hotspot
 
 # Install LogMeIn Hamachi
 
-wget https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb
+wget https://github.com/Ivan-Alone/imageres-storage/raw/master/logmein-hamachi_2.1.0.203-1_amd64.deb
 dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
 rm -rf logmein-hamachi_2.1.0.203-1_amd64.deb
 
