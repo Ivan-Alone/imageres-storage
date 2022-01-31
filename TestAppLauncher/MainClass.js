567010e5743df01a2a1bef790639320c185ea3b5
Ivan <ivan-my-mail@yandex.com>
2022-02-01 01:08:29 +0300

    Add files via upload

---
+++ TestAppLauncher/MainClass.js
@@ -0,0 +1,3 @@
+console.log('App loaded!!!');
+
+document.body.innerHTML = 'Ооо, вот тебе годная тян<br><img src="https://raw.githubusercontent.com/Ivan-Alone/imageres-storage/master/TestAppLauncher/megumin.jpg">';
